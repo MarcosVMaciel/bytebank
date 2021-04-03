e3b4bd154d700d6849d1b8cfca0eb8fb696b9f05
Marcos Maciel <macielmarcos25@gmail.com>
2021-04-03 16:13:23 -0300

    Enviando transferência para WebAPI - Implementando a requisição post para o HTTP - Salvando uma transferência na web api - Convertendo objeto Dart pra json

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -5,11 +5,54 @@ import 'package:bytebank/models/transacao.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
+final Client httpClient =
+    HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+var baseUrl = Uri.parse('http://192.168.0.104:8080/transactions');
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print(data.toString());
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print(data.toString());
+    return data;
+  }
+}
+
+Future<Transacao> save(Transacao transacao) async {
+  final Map<String, dynamic> transactionMap = {
+    'value': transacao.value,
+    'contact': {
+      'name': transacao.contact.name,
+      'accountNumber': transacao.contact.accounteNumber,
+    }
+  };
+  final String transactionJson = jsonEncode(transactionMap);
+  final Response response = await httpClient.post(baseUrl,
+      headers: {
+        'Content-Type': 'application/json',
+        'password': '1000',
+      },
+      body: transactionJson);
+
+  Map<String, dynamic> json = jsonDecode(response.body);
+  final Map<String, dynamic> contactJson = json['contact'];
+  return Transacao(
+      json['value'],
+      Contact(
+        0,
+        contactJson['name'],
+        contactJson['accountNumber'],
+      ));
+}
+
 Future<List<Transacao>> findAll() async {
-  final Client httpClient =
-      HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
-  var url = Uri.parse('http://192.168.0.104:8080/transactions');
-  final Response response = await httpClient.get(url).timeout(Duration(seconds: 5));
+  final Response response =
+      await httpClient.get(baseUrl).timeout(Duration(seconds: 5));
   final List<dynamic> decodedJson = jsonDecode(response.body);
   final List<Transacao> transacoes = [];
 
@@ -25,18 +68,4 @@ Future<List<Transacao>> findAll() async {
     transacoes.add(transacao);
   }
   return transacoes;
-}
-
-class LoggingInterceptor implements InterceptorContract {
-  @override
-  Future<RequestData> interceptRequest({RequestData data}) async {
-    print(data.toString());
-    return data;
-  }
-
-  @override
-  Future<ResponseData> interceptResponse({ResponseData data}) async {
-    print(data.toString());
-    return data;
-  }
 }
--- lib/screens/contacts/contacts_list.dart
+++ lib/screens/contacts/contacts_list.dart
@@ -2,6 +2,7 @@ import 'package:bytebank/components/progress.dart';
 import 'package:bytebank/database/dao/contact_dao.dart';
 import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contacts/contact_form.dart';
+import 'package:bytebank/screens/transferencia/transacao_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatefulWidget {
@@ -36,7 +37,16 @@ class _ContactsListState extends State<ContactsList> {
               return ListView.builder(
                 itemBuilder: (context, index) {
                   final Contact contact = contacts[index];
-                  return _ContactItem(contact);
+                  return _ContactItem(
+                    contact,
+                    onClick: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => TransacaoForm(contact),
+                        ),
+                      );
+                    },
+                  );
                 },
                 itemCount: contacts.length,
               );
@@ -63,13 +73,18 @@ class _ContactsListState extends State<ContactsList> {
 
 class _ContactItem extends StatelessWidget {
   final Contact contact;
+  final Function onClick;
 
-  _ContactItem(this.contact);
+  _ContactItem(
+    this.contact, {
+    @required this.onClick,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
+        onTap: onClick,
         title: Text(
           contact.name,
           style: TextStyle(fontSize: 24.0),
