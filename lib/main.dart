ed5dcada8d47894a2f88b4509ebdce7c4855154e
Marcos Maciel <macielmarcos25@gmail.com>
2021-04-03 00:32:12 -0300

    Configurando banco de dados - Configurando sqflite - Criando banco de dados e tabela - Adicionando dependencias ao projeto - Salva e buscas no banco de dados - Possibilidades de armazenamento do flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:bytebank/database/app_database.dart';
+import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/dashboard/dashboard.dart';
 import 'package:bytebank/screens/transferencia/lista.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(ByteBankApp());
+  save(Contact(0, 'Pedro', 12355)).then((id) {
+    findAll().then((contacts) => debugPrint(contacts.toString()));
+  });
 }
 
 class ByteBankApp extends StatelessWidget {
--- lib/models/contact.dart
+++ lib/models/contact.dart
@@ -1,11 +1,12 @@
 class Contact {
+  final int id;
   final String name;
   final int accounteNumber;
 
-  Contact(this.name, this.accounteNumber);
+  Contact(this.id, this.name, this.accounteNumber);
 
   @override
   String toString() {
-    return 'Contact{name: $name, accounteNumber: $accounteNumber}';
+    return 'Contact{id: $id, name: $name, accounteNumber: $accounteNumber}';
   }
 }
